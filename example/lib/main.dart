9a44e9b4738996b1da1651865f9869d10b4cd8bd
chrismercredi <christophermercredi@gmail.com>
2023-12-18 20:32:38 -0700

    fix: dependencies, updated readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:example/app_bloc_observer.dart';
+import 'package:example/env.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasestoragemanager/firebasestoragemanager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+// ignore:depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Bloc.observer = const AppBlocObserver();
+
+  // Removes the '#' from the URLs in web applications.
+  usePathUrlStrategy();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -19,6 +30,10 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => UploaderCubit(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          UploaderLocalizationsDelegate(),
+        ],
         title: 'Firebase Storage Manager',
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -34,15 +49,68 @@ class UploaderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      return const SignInPage();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Firebase Storage Manager'),
       ),
       body: Center(
         child: UploaderView(
-          user: FirebaseAuth.instance.currentUser!,
+          user: user,
           isWeb: kIsWeb,
         ),
+      ),
+    );
+  }
+}
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isLoading = false;
+
+  Future<void> _signIn() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      // Replace with your test credentials
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      print(e); // In a real app, handle the error more gracefully
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator()
+            : ElevatedButton(
+                onPressed: _signIn,
+                child: const Text('Sign In with Test Account'),
+              ),
       ),
     );
   }
