6b60f5b50a7ca754b1c07fc474c41303156ed856
chrismercredi <christophermercredi@gmail.com>
2023-12-18 20:01:16 -0700

    fix: dependencies

---
+++ lib/src/l10n/l10n.dart
@@ -0,0 +1,4 @@
+export 'messages_all.dart';
+export 'messages_all_locales.dart';
+export 'messages_en.dart';
+export 'uploader_localizations.dart';
--- lib/src/src.dart
+++ lib/src/src.dart
@@ -1,4 +1,5 @@
 export 'cubit/cubit.dart';
 export 'extensions/extensions.dart';
+export 'l10n/l10n.dart';
 export 'views/views.dart';
 export 'widgets/widgets.dart';
